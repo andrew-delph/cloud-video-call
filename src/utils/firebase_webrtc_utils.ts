14373d9edc1a16f8eca699b988cf55955816308d
Andrew Delph <andrewdelph@live.ca>
2022-12-05 13:35:54 -0500

    create createRoom util function

---
+++ src/utils/firebase_webrtc_utils.ts
@@ -0,0 +1,79 @@
+export const createRoom = async (
+  localStream: MediaStream,
+  remoteStream: MediaStream
+): Promise<string> => {
+  //   const db = firebase.firestore();
+  //   const roomRef = await db.collection("rooms").doc();
+
+  const roomRef: any = {}; // get from db_utils.ts
+
+  const configuration = undefined;
+
+  console.log("Create PeerConnection with configuration: ", configuration);
+  const peerConnection = new RTCPeerConnection();
+
+  localStream.getTracks().forEach((track) => {
+    peerConnection.addTrack(track, localStream);
+  });
+
+  // Code for collecting ICE candidates below
+  const callerCandidatesCollection = roomRef.collection("callerCandidates");
+
+  peerConnection.addEventListener("icecandidate", (event) => {
+    if (!event.candidate) {
+      console.log("Got final candidate!");
+      return;
+    }
+    console.log("Got candidate: ", event.candidate);
+    callerCandidatesCollection.add(event.candidate.toJSON());
+  });
+  // Code for collecting ICE candidates above
+
+  // Code for creating a room below
+  const offer = await peerConnection.createOffer();
+  await peerConnection.setLocalDescription(offer);
+  console.log("Created offer:", offer);
+
+  const roomWithOffer = {
+    offer: {
+      type: offer.type,
+      sdp: offer.sdp,
+    },
+  };
+  await roomRef.set(roomWithOffer);
+  const roomId: string = roomRef.id;
+
+  // Code for creating a room above
+
+  peerConnection.addEventListener("track", (event) => {
+    console.log("Got remote track:", event.streams[0]);
+    event.streams[0].getTracks().forEach((track) => {
+      console.log("Add a track to the remoteStream:", track);
+      remoteStream.addTrack(track);
+    });
+  });
+
+  // Listening for remote session description below
+  roomRef.onSnapshot(async (snapshot: any) => {
+    const data = snapshot.data();
+    if (!peerConnection.currentRemoteDescription && data && data.answer) {
+      console.log("Got remote description: ", data.answer);
+      const rtcSessionDescription = new RTCSessionDescription(data.answer);
+      await peerConnection.setRemoteDescription(rtcSessionDescription);
+    }
+  });
+  // Listening for remote session description above
+
+  // Listen for remote ICE candidates below
+  roomRef.collection("calleeCandidates").onSnapshot((snapshot: any) => {
+    snapshot.docChanges().forEach(async (change: any) => {
+      if (change.type === "added") {
+        let data = change.doc.data();
+        console.log(`Got new remote ICE candidate: ${JSON.stringify(data)}`);
+        await peerConnection.addIceCandidate(new RTCIceCandidate(data));
+      }
+    });
+  });
+
+  return roomId;
+};
