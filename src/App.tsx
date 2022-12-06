decac5eea7ff3db729e4a74095549563732471d7
Andrew Delph <andrewdelph@live.ca>
2022-12-06 14:45:49 -0500

    working example of firebase and webrtc

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,15 @@ import StreamHolder from "./components/StreamHolder/StreamHolder";
 import { Grid } from "@mui/material";
 import { db } from "./firebase";
 import { useState } from "react";
+import { createRoom, joinRoom } from "./utils/firebase_webrtc_utils";
 
 function App() {
-  const [stream, setStream] = useState<MediaStream>();
+  const [localStream, setLocalStream] = useState<MediaStream>();
+
+  const [remoteStream, setRemoteStream] = useState<MediaStream>();
+
+  const [roomId, setRoomId] = useState<string>();
+
   console.log("db", db);
   db.doc("hello/world").set({ hello: "world" });
 
@@ -21,28 +27,66 @@ function App() {
       })
       .then((stream) => {
         console.log("the stream", stream);
-        setStream(stream);
+        setLocalStream(stream);
+        setRemoteStream(new MediaStream());
       });
+  };
+
+  const createRoomButton = () => {
+    if (!localStream || !remoteStream) alert("stream is undefined");
+    createRoom(
+      localStream || new MediaStream(),
+      remoteStream || new MediaStream()
+    ).then((roomId) => {
+      console.log("the roomId", roomId);
+      setRoomId(roomId);
+    });
+  };
+
+  const joinRoomButton = () => {
+    if (!localStream || !remoteStream) alert("stream is undefined");
+    if (!roomId) alert("roomId is undefined");
+
+    joinRoom(
+      roomId || "test",
+      localStream || new MediaStream(),
+      remoteStream || new MediaStream()
+    ).then(() => {
+      console.log("room joined");
+    });
   };
 
   return (
     <div>
       <button onClick={startButton}>Load</button>
-      <Grid container>
-        <button>start</button> <div>roomID</div>
-      </Grid>
-      <Grid container>
-        <input></input>
-        <button>join</button>
-      </Grid>
-      <Grid container>
-        <Grid item>
-          {stream && <StreamHolder title={"Local"} stream={stream} />}
-        </Grid>
-        <Grid item>
-          {stream && <StreamHolder title={"Remote"} stream={stream} />}
-        </Grid>
-      </Grid>
+      {localStream && (
+        <div>
+          <Grid container>
+            <button onClick={createRoomButton}>createRoom</button>{" "}
+            <div>{roomId}</div>
+          </Grid>
+          <Grid container>
+            <input
+              onChange={(e) => {
+                setRoomId(e.target.value);
+              }}
+            ></input>
+            <button onClick={joinRoomButton}>join</button>
+          </Grid>
+          <Grid container>
+            <Grid item>
+              {localStream && (
+                <StreamHolder title={"Local"} stream={localStream} />
+              )}
+            </Grid>
+            <Grid item>
+              {remoteStream && (
+                <StreamHolder title={"Remote"} stream={remoteStream} />
+              )}
+            </Grid>
+          </Grid>
+        </div>
+      )}
     </div>
   );
 }
