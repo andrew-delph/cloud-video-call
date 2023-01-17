9637d30c66fa3c3a6bd527888ba03a9a5c961f21
Andrew Delph <andrewdelph@live.ca>
2023-01-16 22:18:44 -0500

    create TaskSet

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -1,45 +1,89 @@
 import time
 import json
-from locust import task, User, events
+from locust import task, User, events, TaskSet
 import socketio
 
-class SocketIOUser(User):
+
+class SocketIOTasks(TaskSet):
+
+    connection_start = None
 
     def on_start(self):
         print("start.")
+        self.sio = socketio.Client(reconnection=False)
+        self.sio.on("connect", self.on_connect)
+        self.sio.on("connect_error", self.on_connect_error)
+        self.sio.on("message", self.on_message)
+        self.sio.on("error", self.on_error)
         self.connect()
 
     def on_stop(self, env=None):
         print("stop.")
         self.disconnect()
 
-    @task
-    def hello_world(self):
-        print("test")
-        time.sleep(0.5)
-    
     def connect(self):
-        self.sio = socketio.Client()
-        self.sio.on('connect', self.on_connect)
-        self.sio.on("message", self.on_message)
-        self.sio.connect(self.host)
+        self.connection_start = time.time()
+        self.sio.connect(self.user.host, transports=["websocket"])
 
     def disconnect(self):
         self.sio.disconnect()
+        self.interrupt(False)
+        print("disconnected.")
 
     def on_connect(self):
         events.request_success.fire(
-            request_type='socketio',
-            name='connect',
-            response_time=1,
-            response_length=0
+            request_type="socketio",
+            name="connection",
+            response_time=time.time() - self.connection_start,
+            response_length=0,
+            exception=None,
+            context=None,
         )
 
-    def on_message(self, data):
-        print("got msg: "+data)
-        events.request_success.fire(
-            request_type='socketio',
-            name='message',
-            response_time=1,
-            response_length=0
+    def on_connect_error(self, data):
+        print("The connection failed!")
+        events.request.fire(
+            request_type="socketio",
+            name="connection",
+            response_time=time.time() - self.connection_start,
+            response_length=0,
+            exception="connection failed",
+            context=None,
         )
+        self.interrupt(False)
+
+    def on_message(self, data):
+        print("got msg: " + data)
+        events.request.fire(
+            request_type="socketio",
+            name="message",
+            response_time=1,
+            response_length=0,
+            context=None,
+            exception=None,
+        )
+
+    def on_error(self, data):
+        events.request_failure.fire(
+            request_type="socketio",
+            name="error",
+            response_time=1,
+            response_length=0,
+            exception="error",
+            context=None,
+        )
+        self.interrupt()
+
+    @task
+    def sleep(self):
+        print("sleeping")
+        time.sleep(5)
+
+    @task
+    def ready(self):
+        print("ready")
+        time.sleep(5)
+
+
+class SocketIOUser(User):
+    tasks = [SocketIOTasks]
