723a7613bf71cf5465a5b402cfff1582901c8dbc
Andrew Delph <andrewdelph@live.ca>
2023-01-18 16:17:19 -0500

    clean up locust

--- locust/locustfile.py
+++ locust/locustfile.py
@@ -9,7 +9,6 @@ class SocketIOTasks(TaskSet):
     connection_start = None
 
     def on_start(self):
-        print("start.")
         self.sio = socketio.Client(reconnection=False)
         self.sio.on("connect", self.on_connect)
         self.sio.on("connect_error", self.on_connect_error)
@@ -19,7 +18,6 @@ class SocketIOTasks(TaskSet):
         self.connect()
 
     def on_stop(self, env=None):
-        print("stop.")
         self.disconnect()
 
     def connect(self):
@@ -27,14 +25,12 @@ class SocketIOTasks(TaskSet):
         self.sio.connect(self.user.host, transports=["websocket"])
 
     def disconnect(self):
-        print("disconnect.")
         # NOTE we can turn off the disconnect handle here to not report the disconnection
         if self.sio.connected:
             self.sio.on("disconnect", lambda: None)
             self.sio.disconnect()
 
     def on_connect(self):
-        print("on_connect")
         events.request.fire(
             request_type="socketio",
             name="connection",
@@ -45,19 +41,17 @@ class SocketIOTasks(TaskSet):
         )
 
     def on_connect_error(self, data):
-        print("on_connect_error: " + data)
         events.request.fire(
             request_type="socketio",
             name="connection",
             response_time=time.time() - self.connection_start,
             response_length=0,
-            exception="connection failed",
+            exception="connection failed"+str(data),
             context=None,
         )
         self.interrupt()
 
     def on_disconnect(self):
-        print("on_disconnect.")
         events.request.fire(
             request_type="socketio",
             name="on_disconnect",
@@ -69,7 +63,8 @@ class SocketIOTasks(TaskSet):
         self.interrupt()
 
     def on_message(self, data):
-        print("got msg: " + data)
+        pass
+        # print("got msg: " + data)
         # events.request.fire(
         #     request_type="socketio",
         #     name="message",
@@ -80,13 +75,12 @@ class SocketIOTasks(TaskSet):
         # )
 
     def on_error(self, data):
-        print("on_error")
         events.request.fire(
             request_type="socketio",
             name="error",
             response_time=1,
             response_length=0,
-            exception="error",
+            exception="error:"+str(data),
             context=None,
         )
         self.interrupt()
@@ -97,7 +91,6 @@ class SocketIOTasks(TaskSet):
 
     @task
     def ready(self):
-        print("ready")
         start_time = time.time()
 
         ready_event = [False]
@@ -120,7 +113,6 @@ class SocketIOTasks(TaskSet):
 
         while ready_event[0] == False:
             if time.time() - start_time > 15:
-                print("Ready Time limit exceeded")
                 break
             time.sleep(1)
 
@@ -133,13 +125,12 @@ class SocketIOTasks(TaskSet):
                 name="ready",
                 response_time=None,
                 response_length=0,
-                exception="not ready event received",
+                exception="time limit exceeded",
                 context=None,
             )
 
     @task
     def ping(self):
-        print("ping")
         ping_event = [False]
 
         def ping_callback(ping_event, start_time):
@@ -158,7 +149,6 @@ class SocketIOTasks(TaskSet):
 
         while ping_event[0] == False:
             if time.time() - start_time > 15:
-                print("Ping Time limit exceeded")
                 break
             time.sleep(1)
 
