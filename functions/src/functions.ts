1c637d3aa6152ea3f12b6f6a76e422ce96411d8c
Andrew Delph <andrewdelph@live.ca>
2023-01-19 14:54:01 -0500

    enable redlock

--- functions/src/functions.ts
+++ functions/src/functions.ts
@@ -112,12 +112,12 @@ exports.readyEvent = functions
   .onDispatch(async (data: any, context: any) => {
     await init;
 
-    // let lock = await redlock.acquire(["a"], 5000);
+    let lock = await redlock.acquire(["a"], 5000);
 
-    // console.log("lock", lock);
-    // console.log("lock.value", lock.value);
+    console.log("lock", lock);
+    console.log("lock.value", lock.value);
 
-    // await lock.release();
+    await lock.release();
 
     const socketID: string = data.id;
 
