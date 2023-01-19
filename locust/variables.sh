1c0ebd1d4120abe8122ba37e0029db585978d488
Andrew Delph <andrewdelph@live.ca>
2023-01-18 19:21:48 -0500

    init for distribute

---
+++ locust/variables.sh
@@ -0,0 +1,21 @@
+echo "setting variables"
+
+export GKE_CLUSTER=gke-locust-cluster
+export AR_REPO=dist-locust-repo
+export REGION=us-central1
+export ZONE=us-central1-b
+
+export GKE_NODE_TYPE=e2-standard-2
+export GKE_SCOPE="https://www.googleapis.com/auth/cloud-platform"
+export PROJECT=$(gcloud config get-value project)
+export SAMPLE_APP_TARGET=${PROJECT}.appspot.com
+
+export SERVICE_ACCOUNT=dist-locust-svc-acc
+
+export LOCUST_IMAGE_NAME=locust-tasks
+export LOCUST_IMAGE_TAG=latest
+
+gcloud config set compute/zone ${ZONE}
+
+
+echo "project: ${PROJECT}"
