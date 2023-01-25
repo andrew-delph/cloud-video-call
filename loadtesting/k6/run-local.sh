e3306d7c94fc20f2edc82c7de5c352c516fb5649
Andrew Delph <andrewdelph@live.ca>
2023-01-25 17:41:54 -0500

    init k6

---
+++ loadtesting/k6/run-local.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+set -e
+
+if [ $# -ne 1 ]; then
+    echo "Usage: ./run-local.sh <SCRIPT_NAME>"
+    exit 1
+fi
+
+IMAGE_NAME=${IMAGE_NAME:="andrewdelph/k6_tests:latest"}
+
+docker run -v $PWD:/scripts -it --rm $IMAGE_NAME run /scripts/$1
