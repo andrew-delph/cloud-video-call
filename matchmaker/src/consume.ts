0d20008432403240a56ae9f9cdd3dca3e5b75114
Andrew Delph <andrewdelph@live.ca>
2023-02-07 14:30:14 -0500

    some kafka tests

--- matchmaker/src/consume.ts
+++ matchmaker/src/consume.ts
@@ -2,5 +2,19 @@ import { Kafka } from "kafkajs";
 
 const kafka = new Kafka({
   clientId: "my-app",
-  brokers: ["kafka:9092"],
+  brokers: ["localhost:9092"],
 });
+
+(async () => {
+  const consumer = kafka.consumer({ groupId: "test-group" });
+
+  await consumer.connect();
+  await consumer.subscribe({ topic: "test-topic", fromBeginning: true });
+
+  await consumer.run({
+    eachMessage: async ({ topic, partition, message }) => {
+      console.log("msg recieved");
+      console.log(message.value?.toString());
+    },
+  });
+})();
--- matchmaker/src/produce.ts
+++ matchmaker/src/produce.ts
@@ -2,5 +2,28 @@ import { Kafka } from "kafkajs";
 
 const kafka = new Kafka({
   clientId: "my-app",
-  brokers: ["kafka1:9092", "kafka2:9092"],
+  brokers: ["localhost:9092"],
 });
+
+(async () => {
+  const producer = kafka.producer();
+  const admin = kafka.admin();
+
+  //   await admin.connect();
+
+  //   await admin.createTopics({
+  //     waitForLeaders: true,
+  //     timeout: 1000,
+  //     topics: [{ topic: "test-topic" }],
+  //   });
+
+  //   await admin.disconnect();
+
+  await producer.connect();
+  await producer.send({
+    topic: "test-topic",
+    messages: [{ value: "Hello KafkaJS user!" }],
+  });
+
+  await producer.disconnect();
+})();
