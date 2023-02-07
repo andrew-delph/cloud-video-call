7165d4f9e0f6b4e90e13b781522012ef7c0db536
Andrew Delph <andrewdelph@live.ca>
2023-02-07 15:30:33 -0500

    update images.sh

--- images.sh
+++ images.sh
@@ -3,7 +3,9 @@
 docker build -t andrewdelph/video-call-nginx:latest ./nginx
 docker build -t andrewdelph/video-call-websocket:latest -f ./server/. .
 docker build -t andrewdelph/video-call-matcher:latest -f ./functions/. .
+docker build -t andrewdelph/video-call-matchmaker:latest -f ./matchmaker/. .
 
 docker push andrewdelph/video-call-nginx:latest 
 docker push andrewdelph/video-call-websocket:latest 
 docker push andrewdelph/video-call-matcher:latest 
+docker push andrewdelph/video-call-matchmaker:latest 
