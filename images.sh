cc466cac2f983a682bb4f5bb72fb2e1d480cf142
Andrew Delph <andrewdelph@live.ca>
2023-02-09 17:54:08 -0500

    change dockerfile

--- images.sh
+++ images.sh
@@ -2,9 +2,9 @@
 
 # Build
 docker build -t andrewdelph/video-call-nginx:latest ./nginx
-docker build -t andrewdelph/video-call-websocket:latest -f ./websocket/. .
-docker build -t andrewdelph/video-call-matcher:latest -f ./matcher/. .
-docker build -t andrewdelph/video-call-matchmaker:latest -f ./matchmaker/. .
+docker build -t andrewdelph/video-call-websocket:latest -f ./websocket/Dockerfile .
+docker build -t andrewdelph/video-call-matcher:latest -f ./matcher/Dockerfile .
+docker build -t andrewdelph/video-call-matchmaker:latest -f ./matchmaker/Dockerfile .
 
 docker push andrewdelph/video-call-nginx:latest 
 docker push andrewdelph/video-call-websocket:latest 
