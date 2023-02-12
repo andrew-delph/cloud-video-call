3d261615bedbbb4b900699b70a869aa7a9105b0f
Andrew Delph <andrewdelph@live.ca>
2023-02-12 13:56:53 -0500

    allow gds

--- neo4j/src/index.ts
+++ neo4j/src/index.ts
@@ -23,7 +23,7 @@ function printResults(result: any) {
     // for (var i = 0; i < 3; i++) {
     //   result = await funcs.changeRandomReady()
     // }
-    result = await funcs.getFirstN();
+    result = await funcs.testGraph();
     // await funcs.testGraph();
     printResults(result);
   } finally {
