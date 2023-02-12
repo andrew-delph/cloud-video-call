8e86594bec3da969e62ff7f672727d9e539d480a
Andrew Delph <andrewdelph@live.ca>
2023-02-12 13:49:47 -0500

    update pretty

--- neo4j/src/index.ts
+++ neo4j/src/index.ts
@@ -1,36 +1,36 @@
-import * as neo4j from 'neo4j-driver'
-import * as funcs from './functions'
+import * as neo4j from 'neo4j-driver';
+import * as funcs from './functions';
 
-let result
+let result;
 
 function printResults(result: any) {
   //   console.log("Results:");
   //   console.log(result.records);
-  console.log(`Summary:`)
-  console.log(``)
-  console.log(result.summary)
-  const records = result.records
-  const limit = 10
-  console.log(`print records. limit is ${limit}`)
+  console.log(`Summary:`);
+  console.log(``);
+  console.log(result.summary);
+  const records = result.records;
+  const limit = 10;
+  console.log(`print records. limit is ${limit}`);
   records.slice(0, 10).forEach((record: any) => {
-    console.log(record.get(`n`))
-  })
-  console.log(`records.length:`, records.length)
+    console.log(record.get(`n`));
+  });
+  console.log(`records.length:`, records.length);
 }
 
-;(async () => {
+(async () => {
   try {
     // for (var i = 0; i < 3; i++) {
     //   result = await funcs.changeRandomReady()
     // }
-    result = await funcs.getAllReady()
+    result = await funcs.getFirstN();
     // await funcs.testGraph();
-    printResults(result)
+    printResults(result);
   } finally {
-    console.log(`closing`)
-    await funcs.session.close()
-    await funcs.driver.close()
+    console.log(`closing`);
+    await funcs.session.close();
+    await funcs.driver.close();
   }
 
   // on application exit:
-})()
+})();
