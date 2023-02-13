55e5efd6a9488ecf1c2e1d75739e9ce767000b99
Andrew Delph <andrewdelph@live.ca>
2023-02-13 17:35:33 -0500

    websocket creates node on connection

--- websocket/src/server.ts
+++ websocket/src/server.ts
@@ -7,6 +7,7 @@ import { Server } from 'socket.io';
 import { v4 as uuid } from 'uuid';
 // import { RedisAdapter } from "@socket.io/redis-adapter";
 import * as common from 'react-video-call-common';
+import * as neo4j from 'neo4j-driver';
 
 import { initializeApp } from 'firebase-admin/app';
 
@@ -28,14 +29,19 @@ let rabbitChannel: amqp.Channel;
 //   console.log("rabbit connected");
 // };
 
+export const driver = neo4j.driver(
+  `neo4j://neo4j:7687`,
+  neo4j.auth.basic(`neo4j`, `password`),
+);
+
 const connectKafkaProducer = async () => {
-    const kafka = new Kafka({
-        clientId: `my-app`,
-        brokers: [`my-cluster-kafka-bootstrap:9092`],
-    });
-    kafkaProducer = kafka.producer();
-    await kafkaProducer.connect();
-    console.log(`kafka producer connected`);
+  const kafka = new Kafka({
+    clientId: `my-app`,
+    brokers: [`my-cluster-kafka-bootstrap:9092`],
+  });
+  kafkaProducer = kafka.producer();
+  await kafkaProducer.connect();
+  console.log(`kafka producer connected`);
 };
 
 // const queueReadyEvent = async (message: string) => {
@@ -59,122 +65,129 @@ const app = express();
 const httpServer = createServer(app);
 
 const io = new Server(httpServer, {
-    cors: {
-        origin: `*`,
-        methods: [`GET`, `POST`],
-    },
+  cors: {
+    origin: `*`,
+    methods: [`GET`, `POST`],
+  },
 });
 
 const pubClient = createClient({
-    url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
-    name: `websocket`,
+  url: `redis://${process.env.REDIS_HOST}:${process.env.REDIS_PORT}`,
+  name: `websocket`,
 });
 
 pubClient.on(`error`, function (error) {
-    console.error(error);
+  console.error(error);
 });
 
 const subClient = pubClient.duplicate();
 
 app.get(`*`, (req, res) => {
-    res.send(`This is the API server :)`);
+  res.send(`This is the API server :)`);
 });
 
 io.on(`error`, (err) => {
-    console.log(`io err`, err);
+  console.log(`io err`, err);
 });
 
 io.on(`connection`, async (socket) => {
-    socket.on(`error`, (err) => {
-        console.log(`socket err`, err);
+  socket.on(`error`, (err) => {
+    console.log(`socket err`, err);
+  });
+
+  console.log(`connection on ${serverID} ${socket.id}`);
+
+  await (async () => {
+    const session = driver.session();
+    await session.run(`MERGE (:Person {socketid: $id})`, {
+      id: socket.id,
     });
+  })();
+  // console.log("connected");
 
-    console.log(`connection on ${serverID} ${socket.id}`);
-    // console.log("connected");
+  pubClient.sAdd(common.activeSetName, socket.id);
 
-    pubClient.sAdd(common.activeSetName, socket.id);
-
-    socket.on(`myping`, (arg, callback) => {
+  socket.on(`myping`, (arg, callback) => {
     // console.log("myping", arg, callback);
-        try {
-            if (callback != undefined) callback(`ping ding dong`);
-        } catch (e) {
-            console.error(e);
-        }
+    try {
+      if (callback != undefined) callback(`ping ding dong`);
+    } catch (e) {
+      console.error(e);
+    }
+  });
+
+  socket.on(`message`, (msg) => {
+    console.log(`message:`, msg);
+  });
+
+  socket.on(`ready`, async (data, callback) => {
+    pubClient.sAdd(common.readySetName, socket.id);
+
+    await kafkaProducer.send({
+      topic: `test-topic`,
+      messages: [{ value: socket.id }],
     });
 
-    socket.on(`message`, (msg) => {
-        console.log(`message:`, msg);
-    });
+    if (callback != undefined) {
+      callback();
+    }
+  });
 
-    socket.on(`ready`, async (data, callback) => {
-        pubClient.sAdd(common.readySetName, socket.id);
+  // let updateCount = 0;
+  // const myInterval = setInterval(async () => {
+  //   updateCount = updateCount + 1;
+  //   socket.emit("message", `updateCount: ${updateCount}`);
+  // }, 5000);
 
-        await kafkaProducer.send({
-            topic: `test-topic`,
-            messages: [{ value: socket.id }],
-        });
-
-        if (callback != undefined) {
-            callback();
-        }
-    });
-
-    // let updateCount = 0;
-    // const myInterval = setInterval(async () => {
-    //   updateCount = updateCount + 1;
-    //   socket.emit("message", `updateCount: ${updateCount}`);
-    // }, 5000);
-
-    socket.on(`disconnect`, async () => {
+  socket.on(`disconnect`, async () => {
     // console.log("disconnected");
     // clearInterval(myInterval);
-        pubClient.sRem(common.activeSetName, socket.id);
-        pubClient.sRem(common.readySetName, socket.id);
-        pubClient.publish(common.activeCountChannel, `change`);
-    });
-
-    socket.on(`client_host`, (value) => {
-        socket.to(`room-${socket.id}`).emit(`client_guest`, value);
-    });
-
-    socket.on(`client_guest`, (value) => {
-        socket.to(`room-${socket.id}`).emit(`client_host`, value);
-    });
-
-    socket.on(`icecandidate`, (value) => {
-        socket.to(`room-${socket.id}`).emit(`icecandidate`, value);
-    });
-
+    pubClient.sRem(common.activeSetName, socket.id);
+    pubClient.sRem(common.readySetName, socket.id);
     pubClient.publish(common.activeCountChannel, `change`);
+  });
 
-    socket.emit(`message`, `Hey from server :) I am ${serverID}.`);
+  socket.on(`client_host`, (value) => {
+    socket.to(`room-${socket.id}`).emit(`client_guest`, value);
+  });
 
-    // setTimeout(() => {
-    //   socket.timeout(1000).emit("myping", "hello", (err: any, response: any) => {
-    //     if (err) {
-    //       console.error("err", err);
-    //     } else {
-    //       // console.log("response", response);
-    //     }
-    //   });
-    // }, 1000);
+  socket.on(`client_guest`, (value) => {
+    socket.to(`room-${socket.id}`).emit(`client_host`, value);
+  });
+
+  socket.on(`icecandidate`, (value) => {
+    socket.to(`room-${socket.id}`).emit(`icecandidate`, value);
+  });
+
+  pubClient.publish(common.activeCountChannel, `change`);
+
+  socket.emit(`message`, `Hey from server :) I am ${serverID}.`);
+
+  // setTimeout(() => {
+  //   socket.timeout(1000).emit("myping", "hello", (err: any, response: any) => {
+  //     if (err) {
+  //       console.error("err", err);
+  //     } else {
+  //       // console.log("response", response);
+  //     }
+  //   });
+  // }, 1000);
 });
 
 Promise.all([pubClient.connect(), subClient.connect()])
-    .then(async () => {
-        await connectKafkaProducer();
-    })
-    .then(() => {
-        io.adapter(createAdapter(pubClient, subClient));
-        httpServer.listen(4000);
-        console.log(`server started`);
-    })
-    .then(() => {
-        subClient.subscribe(
-            common.activeCountChannel,
-            throttle(async (msg) => {
-                io.emit(`activeCount`, await pubClient.sCard(common.activeSetName));
-            }, 1000)
-        );
-    });
+  .then(async () => {
+    await connectKafkaProducer();
+  })
+  .then(() => {
+    io.adapter(createAdapter(pubClient, subClient));
+    httpServer.listen(4000);
+    console.log(`server started`);
+  })
+  .then(() => {
+    subClient.subscribe(
+      common.activeCountChannel,
+      throttle(async (msg) => {
+        io.emit(`activeCount`, await pubClient.sCard(common.activeSetName));
+      }, 1000),
+    );
+  });
