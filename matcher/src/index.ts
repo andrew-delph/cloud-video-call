c1bd9c9902bcb50b2bbb1cc3c5ee78f4210e6bb0
Andrew Delph <andrewdelph@live.ca>
2023-02-14 15:00:41 -0500

    matcher always ack

--- matcher/src/index.ts
+++ matcher/src/index.ts
@@ -3,37 +3,37 @@ import { readyEvent } from './functions';
 import * as common from 'react-video-call-common';
 
 async function matcher() {
-    const connection = await connect(`amqp://rabbitmq`);
+  const connection = await connect(`amqp://rabbitmq`);
 
-    const channel = await connection.createChannel();
+  const channel = await connection.createChannel();
 
-    await channel.assertQueue(common.readyQueueName, {
-        durable: true,
-    });
+  await channel.assertQueue(common.readyQueueName, {
+    durable: true,
+  });
 
-    // channel.prefetch(10);
-    console.log(` [x] Awaiting RPC requests`);
+  // channel.prefetch(10);
+  console.log(` [x] Awaiting RPC requests`);
 
-    channel.consume(
-        common.readyQueueName,
-        async (msg: ConsumeMessage | null) => {
-            if (msg == null) {
-                console.log(`msg is null.`);
-                return;
-            }
+  channel.consume(
+    common.readyQueueName,
+    async (msg: ConsumeMessage | null) => {
+      if (msg == null) {
+        console.log(`msg is null.`);
+        return;
+      }
 
-            try {
-                await readyEvent(msg, channel);
-                channel.ack(msg);
-            } catch (e) {
-                console.log(`readyEvent error=` + e);
-                channel.nack(msg, false, false);
-            }
-        },
-        {
-            noAck: false,
-        }
-    );
+      try {
+        await readyEvent(msg, channel);
+      } catch (e) {
+        console.log(`readyEvent error=` + e);
+      } finally {
+        channel.ack(msg);
+      }
+    },
+    {
+      noAck: false,
+    },
+  );
 }
 
 matcher();
