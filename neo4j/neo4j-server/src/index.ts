81bf80fdd37a3ff3176ea99c8afb48ab04a1ffb6
Andrew Delph <andrewdelph@live.ca>
2023-02-17 09:44:42 -0500

    clean ups

--- neo4j/neo4j-server/src/index.ts
+++ neo4j/neo4j-server/src/index.ts
@@ -1,2 +1,23 @@
-console.log(`hi`);
-// npx grpc_tools_node_protoc --js_out=import_style=commonjs,binary:./ --grpc_out=./ --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` ./path/to/proto/file.proto
+import * as grpc from 'grpc';
+import {
+  Greeter,
+  HelloRequest,
+  HelloResponse,
+} from '../proto-gen/proto/greeter';
+
+function sayHello(
+  call: grpc.ServerUnaryCall<HelloRequest>,
+  callback: grpc.sendUnaryData<HelloResponse>,
+) {
+  const name = call.;
+  const message = `Hello, ${name}!`;
+  const response = new HelloResponse.();
+  response.setMessage(message);
+  callback(null, response);
+}
+
+const server = new grpc.Server();
+server.addService(Greeter, { sayHello });
+server.bind(`0.0.0.0:50051`, grpc.ServerCredentials.createInsecure());
+console.log(`Server running at http://0.0.0.0:50051`);
+server.start();
