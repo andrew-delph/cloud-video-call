b416e8d78e02b12b644e9f8b4928dc927c7f0971
Andrew Delph <andrewdelph@live.ca>
2023-02-17 09:29:18 -0500

    build proto

--- neo4j/neo4j-server/src/index.ts
+++ neo4j/neo4j-server/src/index.ts
@@ -1,1 +1,2 @@
 console.log(`hi`);
+// npx grpc_tools_node_protoc --js_out=import_style=commonjs,binary:./ --grpc_out=./ --plugin=protoc-gen-grpc=`which grpc_tools_node_protoc_plugin` ./path/to/proto/file.proto
