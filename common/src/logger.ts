a09da8692ebdbec1dad23350c3c55e070b3f5217
Andrew Delph <andrewdelph@live.ca>
2023-02-21 12:09:35 -0500

    show debug

--- common/src/logger.ts
+++ common/src/logger.ts
@@ -1,6 +1,7 @@
 import { createLogger, transports, format, config } from 'winston';
 export const getLogger = () => {
   return createLogger({
+    level: `debug`,
     // levels: config.syslog.levels,
     format: format.combine(
       //   format.label({ label: `right meow!` }),
