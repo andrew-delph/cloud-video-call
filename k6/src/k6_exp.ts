c802276107d9f82cf85f8e1972bdfcbfd64d92f0
Andrew Delph <andrewdelph@live.ca>
2023-02-28 14:09:41 -0500

    small test

--- k6/src/k6_exp.ts
+++ k6/src/k6_exp.ts
@@ -7,7 +7,7 @@ import { check, sleep } from 'k6';
 const vus = 50;
 export const options = {
   vus: 20,
-  duration: `2m`,
+  duration: `20s`,
   // scenarios: {
   //   matchTest: {
   //     executor: `ramping-vus`,
