bb6522dc709dcfdd54990cb9da75dd7df564b957
Andrew Delph <andrewdelph@live.ca>
2023-02-28 17:23:39 -0500

    update test

--- k6/src/k6_exp.ts
+++ k6/src/k6_exp.ts
@@ -49,7 +49,7 @@ const success_counter = new Counter(`success_counter`);
 const redisClient = new redis.Client({
   addrs: new Array(`redis.default.svc.cluster.local:6379`), // in the form of 'host:port', separated by commas
 });
-const authKeysNum = 299;
+const authKeysNum = 10000;
 const authKeysName = `authKeysName`;
 export function setup() {
   const authKeys: string[] = [];
