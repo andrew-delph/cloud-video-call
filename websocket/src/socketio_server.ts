13293765dc8db54982d7d396465b26744dfc3445
Andrew Delph <andrewdelph@live.ca>
2023-03-03 12:58:32 -0500

    clean room on disconnect

--- websocket/src/socketio_server.ts
+++ websocket/src/socketio_server.ts
@@ -114,6 +114,7 @@ io.on(`connection`, async (socket) => {
 
   socket.on(`disconnect`, async () => {
     socket.to(`room-${socket.id}`).emit(`endchat`, `disconnected`);
+    io.socketsLeave(`room-${socket.id}`);
     const duration = performance.now() - start_time;
     logger.debug(
       `disconnected ${process.env.HOSTNAME} #${
