076d2f6a64d98c190a2273bd0a998befc5873497
Andrew Delph <andrewdelph@live.ca>
2023-03-15 16:34:46 -0400

    clean neo4j

--- neo4j/install.sh
+++
@@ -1,6 +0,0 @@
-
-echo "installing helm"
-
-helm upgrade --install  neo4j neo4j-helm/neo4j --version 4.1.0-3 --set core.standalone=true --set acceptLicenseAgreement=yes --set neo4jPassword=password -f k8.values.yaml
-
-kubectl rollout status --watch --timeout=600s statefulset/neo4j
