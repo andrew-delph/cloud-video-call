200bc5743d28f7a799b22583c4d113ca7e77e139
Andrew Delph <andrewdelph@live.ca>
2023-03-18 13:15:37 -0400

    add js_library

--- matcher/test.ts
+++ matcher/test.ts
@@ -1,8 +1,6 @@
 console.log(`hi`);
-// import * as common from 'react-video-call-common';
 import express from 'express';
-import * as common from 'common/test';
 
-// import * as common from 'react-video-call-common';
+import * as common from 'common';
 
-console.log(`common`, common.mytest);
+console.log(`common`, common.activeSetName);
