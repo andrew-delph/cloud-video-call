a486e55798bda6d86c031f687edf81231ca5faa6
Andrew Delph <andrewdelph@live.ca>
2023-03-20 12:49:14 -0400

    common doesnt initalize the firebase app

--- common/src/utils.ts
+++ common/src/utils.ts
@@ -1,10 +1,7 @@
 import { getLogger } from './logger';
-import { initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 
 const logger = getLogger();
-
-const firebaseApp = initializeApp();
 
 export function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
