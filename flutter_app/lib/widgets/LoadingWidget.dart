c76a510c0d774f3289f88dfc7bde79b53044ada5
Andrew Delph <andrewdelph@live.ca>
2023-03-28 12:00:34 -0400

    fix render box error

--- flutter_app/lib/widgets/LoadingWidget.dart
+++ flutter_app/lib/widgets/LoadingWidget.dart
@@ -2,26 +2,23 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:loading_animation_widget/loading_animation_widget.dart';
 
-Widget connectingWidget = Scaffold(
-    body: Center(
+Widget connectingWidget = Center(
   child: LoadingAnimationWidget.staggeredDotsWave(
     color: Colors.blue,
     size: 400,
   ),
-));
+);
 
-Widget loadingWidget = Scaffold(
-    body: Center(
+Widget loadingWidget = Center(
   child: LoadingAnimationWidget.staggeredDotsWave(
     color: Colors.green,
     size: 400,
   ),
-));
+);
 
-Widget errorWidget = Scaffold(
-    body: Center(
+Widget errorWidget = Center(
   child: LoadingAnimationWidget.staggeredDotsWave(
     color: Colors.red,
     size: 400,
   ),
-));
+);
