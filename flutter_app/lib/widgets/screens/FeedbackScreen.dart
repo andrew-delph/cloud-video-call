33bfb2b9a1429fc89bf6f9515e4feaee62d8bc8d
Andrew Delph <andrewdelph@live.ca>
2023-03-28 10:54:04 -0400

    feedbackscreen can swipe

--- flutter_app/lib/widgets/screens/FeedbackScreen.dart
+++ flutter_app/lib/widgets/screens/FeedbackScreen.dart
@@ -1,14 +1,7 @@
-import 'dart:convert';
-
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/state_machines.dart';
-import 'package:flutter_app/utils.dart';
 
 import '../../AppProvider.dart';
-import '../../Factory.dart';
-
-import 'package:http/http.dart' as http;
 
 class FeedbackScreen extends StatefulWidget {
   final String label;
@@ -29,6 +22,7 @@ class FeedbackScreen extends StatefulWidget {
 
 class FeedbackScreenState extends State<FeedbackScreen> {
   late int _value;
+  double _positionX = 0;
 
   @override
   void initState() {
@@ -36,9 +30,25 @@ class FeedbackScreenState extends State<FeedbackScreen> {
     _value = widget.initialValue;
   }
 
+  Future<void> sendScore(score) {
+    return widget.appProvider
+        .sendChatScore(score)
+        .then((value) {})
+        .catchError((error) {
+      SnackBar snackBar = SnackBar(
+        content: Text(error.toString()),
+      );
+
+      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      Navigator.of(context).pop();
+    }).whenComplete(() {
+      widget.appProvider.chatMachine.current = ChatStates.waiting;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
+    Widget page = Column(
       children: [
         Text(widget.label),
         Slider(
@@ -53,23 +63,39 @@ class FeedbackScreenState extends State<FeedbackScreen> {
         ),
         ElevatedButton(
           onPressed: () {
-            widget.appProvider
-                .sendChatScore(_value.toDouble())
-                .then((value) {})
-                .catchError((error) {
-              SnackBar snackBar = SnackBar(
-                content: Text(error.toString()),
-              );
-
-              ScaffoldMessenger.of(context).showSnackBar(snackBar);
-              Navigator.of(context).pop();
-            }).whenComplete(() {
-              widget.appProvider.chatMachine.current = ChatStates.waiting;
-            });
+            sendScore(_value.toDouble());
           },
           child: const Text('Submit'),
         ),
       ],
     );
+
+    page = GestureDetector(
+      onHorizontalDragUpdate: (details) {
+        setState(() {
+          _positionX += details.delta.dx;
+        });
+      },
+      onHorizontalDragEnd: (details) {
+        print("onHorizontalDragEnd _positionX $_positionX");
+        if (_positionX.abs() > 50) {
+          double score = 0;
+          if (_positionX > 0) {
+            score = 5;
+          }
+          sendScore(score);
+        }
+        setState(() {
+          _positionX = 0;
+        });
+      },
+      child: AnimatedContainer(
+        duration: const Duration(milliseconds: 200),
+        transform: Matrix4.translationValues(_positionX, 0, 0),
+        child: page,
+      ),
+    );
+
+    return page;
   }
 }
