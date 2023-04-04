78620cb0c75021bee1f5f13dbd0f08c381b4638c
Andrew Delph <andrewdelph@live.ca>
2023-04-04 14:39:25 -0400

    Add feedback score to swipe detector

--- flutter_app/lib/widgets/SwipeDetector.dart
+++ flutter_app/lib/widgets/SwipeDetector.dart
@@ -1,6 +1,6 @@
 import 'dart:math';
 
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class SwipeDetector extends StatefulWidget {
   final Widget child;
@@ -25,37 +25,61 @@ class SwipeDetectorState extends State<SwipeDetector> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
+    calcScore() {
+      double score = (_positionX / screenWidth) * 10;
+
+      score = max(score, -10);
+      score = min(score, 10);
+      return score;
+    }
+
+    isValidScore(double score) {
+      return score < -1 || score > 1;
+    }
+
+    double score = calcScore();
+    bool validScore = isValidScore(score);
+
     return GestureDetector(
-      onHorizontalDragUpdate: (details) {
-        if (widget.isDragUpdate == null || widget.isDragUpdate!()) {
-          setState(() {
-            _positionX += details.delta.dx;
-          });
-        }
-      },
-      onHorizontalDragEnd: (details) {
-        if (widget.isDragUpdate == null || widget.isDragUpdate!()) {
-          double screenWidth = MediaQuery.of(context).size.width;
-          print("width $screenWidth _positionX $_positionX");
-
-          double score = (_positionX / screenWidth) * 10;
-
-          score = max(score, -10);
-          score = min(score, 10);
-
-          if (score < -1 || score > 1) {
-            widget.onHorizontalDragEnd(score);
+        onHorizontalDragUpdate: (details) {
+          if (widget.isDragUpdate == null || widget.isDragUpdate!()) {
+            setState(() {
+              _positionX += details.delta.dx;
+            });
           }
-        }
-        setState(() {
-          _positionX = 0;
-        });
-      },
-      child: AnimatedContainer(
-        duration: const Duration(milliseconds: 200),
-        transform: Matrix4.translationValues(_positionX, 0, 0),
-        child: widget.child,
-      ),
-    );
+        },
+        onHorizontalDragEnd: (details) {
+          if (widget.isDragUpdate == null || widget.isDragUpdate!()) {
+            if (validScore) {
+              widget.onHorizontalDragEnd(score);
+            }
+          }
+          setState(() {
+            _positionX = 0;
+          });
+        },
+        child: Stack(
+          children: [
+            AnimatedContainer(
+                duration: const Duration(milliseconds: 200),
+                transform: Matrix4.translationValues(_positionX, 0, 0),
+                child: widget.child),
+            Positioned.fill(
+                child: Align(
+              alignment: Alignment.center,
+              child: validScore
+                  ? Text(
+                      "Feedback=${score.toInt()}",
+                      style: TextStyle(
+                        color: score > 0 ? Colors.green : Colors.red,
+                        fontSize: 70,
+                      ),
+                    )
+                  : Container(),
+            ))
+          ],
+        ));
   }
 }
