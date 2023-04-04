9ec2d539f5b9b6404bab37d74b9f4e599a9b81dd
Andrew Delph <andrewdelph@live.ca>
2023-04-04 17:16:08 -0400

    use unified-plan

--- flutter_app/lib/Factory.dart
+++ flutter_app/lib/Factory.dart
@@ -1,7 +1,7 @@
 import 'package:flutter_webrtc/flutter_webrtc.dart' as webrtc;
 
 final Map<String, dynamic> rtcConfiguration = {
-  "sdpSemantics": "plan-b",
+  "sdpSemantics": "unified-plan", //"plan-b",
   "iceServers": [
     // {
     //   "urls": [
