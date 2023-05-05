5a7aa19ab2ba6fde768d6178b01a9e1ddddc0fe0
Andrew Delph <andrewdelph@live.ca>
2023-05-05 14:25:27 -0400

    include month and day in k6 test

--- k6/run-kube.sh
+++ k6/run-kube.sh
@@ -16,7 +16,7 @@ SCRIPT_NAME="$(yq -r '.spec.script.configMap.file' $RESOURCE_FILENAME)"
 echo "SCRIPT_NAME:" $SCRIPT_NAME
 # Clean up the script name and append the unique timestamp
 TAG_PREFIX="$(basename -s .js $SCRIPT_NAME)"
-TAG_NAME="test-$(date +%H-%M-%S)"
+TAG_NAME="test-$(date +%m-%d-%H-%M-%S)"
 echo "test id:" $TAG_NAME
 
 # Replacement doesn't seem to trigger, so we need to delete any previous execution
