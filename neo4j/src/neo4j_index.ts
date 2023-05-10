263346088ce18c387bd2828831ecc896bcfbdd9b
Andrew Delph <andrewdelph@live.ca>
2023-05-10 16:02:01 -0400

    goo predictions with similair

--- neo4j/src/neo4j_index.ts
+++ neo4j/src/neo4j_index.ts
@@ -51,35 +51,170 @@ const start_time = performance.now();
 export const run = async () => {
   try {
     funcs.setDriver(`bolt://localhost:7687`);
-    // results = await funcs
-    //   .compareTypes
-    //   // ``,
-    //   // ``,
-    //   // `{gender:'male'}`,
-    //   // `{gender:'female'}`,
-    //   ();
 
-    const otherIds: string[] = [];
-    console.log(`...`);
+    await funcs.createData({
+      deleteData: true,
+      nodesNum: 100,
+      edgesNum: 10,
+    });
+    results = await funcs.createFriends();
+    const test_attributes: string[] = await funcs.getAttributeKeys();
+    results = await funcs.createGraph(`myGraph`, test_attributes);
 
-    // for (let i = 0; i < 30; i++) {
-    //   otherIds.push(`node${i}`);
-    // }
-
-    results = await funcs.createGraph(
-      `test`,
-      await funcs.getAttributeKeys(),
-      otherIds,
+    results = await funcs.run(
+      `
+      CALL gds.fastRP.write('myGraph',
+      {
+        nodeLabels: ['Person'],
+        relationshipTypes: ['FRIENDS', 'NEGATIVE'],
+        featureProperties: ['values'],
+        embeddingDimension: 256,
+        writeProperty: 'embedding'
+      }
+      )
+    `,
     );
 
-    results = await lp.predict(false, `test`);
+    // SIMILAR0 write
+    results = await funcs.run(
+      `
+      CALL gds.nodeSimilarity.write('myGraph', {
+        nodeLabels: ['Person'],
+        relationshipTypes: ['FRIENDS','NEGATIVE'],
+        writeRelationshipType: 'SIMILAR0',
+        writeProperty: 'score'
+      })
+      YIELD nodesCompared, relationshipsWritten
+    `,
+    );
 
-    results.records.forEach((record) => {
-      const userId1: string = record.get(`person1.userId`);
-      const userId2: string = record.get(`person2.userId`);
-      const probability: number = record.get(`probability`);
-      console.log(userId1, userId2, probability);
-    });
+    // SIMILAR1 write
+    results = await funcs.run(
+      `
+      CALL gds.nodeSimilarity.write('myGraph', {
+        nodeLabels: ['Person'],
+        relationshipTypes: ['FRIENDS'],
+        writeRelationshipType: 'SIMILAR1',
+        writeProperty: 'score'
+      })
+      YIELD nodesCompared, relationshipsWritten
+    `,
+    );
+
+    // SIMILAR1 mutate
+    results = await funcs.run(
+      `
+      CALL gds.nodeSimilarity.mutate('myGraph', {
+        nodeLabels: ['Person'],
+        relationshipTypes: ['FRIENDS'],
+        mutateRelationshipType: 'SIMILAR1',
+        mutateProperty: 'score'
+      })
+      YIELD nodesCompared, relationshipsWritten
+    `,
+    );
+
+    // SIMILAR2 write
+    results = await funcs.run(
+      `
+      CALL gds.nodeSimilarity.write('myGraph', {
+        nodeLabels: ['Person'],
+        relationshipTypes: ['FRIENDS', 'SIMILAR1'],
+        writeRelationshipType: 'SIMILAR2',
+        writeProperty: 'score'
+      })
+      YIELD nodesCompared, relationshipsWritten
+    `,
+    );
+
+    // SIMILAR3 mutate
+    results = await funcs.run(
+      `
+      CALL gds.nodeSimilarity.mutate('myGraph', {
+        nodeLabels: ['Person'],
+        relationshipTypes: ['NEGATIVE'],
+        mutateRelationshipType: 'SIMILAR3',
+        mutateProperty: 'score'
+      })
+      YIELD nodesCompared, relationshipsWritten
+    `,
+    );
+
+    // SIMILAR3 write
+    results = await funcs.run(
+      `
+      CALL gds.nodeSimilarity.write('myGraph', {
+        nodeLabels: ['Person'],
+        relationshipTypes: ['NEGATIVE'],
+        writeRelationshipType: 'SIMILAR3',
+        writeProperty: 'score'
+      })
+      YIELD nodesCompared, relationshipsWritten
+    `,
+    );
+
+    // SIMILAR4 write
+    results = await funcs.run(
+      `
+      CALL gds.nodeSimilarity.write('myGraph', {
+        nodeLabels: ['Person'],
+        relationshipTypes: ['NEGATIVE','SIMILAR3'],
+        writeRelationshipType: 'SIMILAR4',
+        writeProperty: 'score'
+      })
+      YIELD nodesCompared, relationshipsWritten
+    `,
+    );
+
+    results = await funcs.run(
+      `
+      MATCH (n1:Person)-[:USER_ATTRIBUTES_CONSTANT]->(md1:MetaData)
+      MATCH (n2:Person)-[:USER_ATTRIBUTES_CONSTANT]->(md2:MetaData)
+      OPTIONAL MATCH (n1)-[srel0:SIMILAR0]->(n2)
+      OPTIONAL MATCH (n1)-[srel1:SIMILAR1]->(n2)
+      OPTIONAL MATCH (n1)-[srel2:SIMILAR2]->(n2)
+      OPTIONAL MATCH (n1)-[srel3:SIMILAR3]->(n2)
+      OPTIONAL MATCH (n1)-[srel4:SIMILAR4]->(n2)
+      WITH n1, n2, md1, md2,
+      gds.similarity.cosine(
+        n1.embedding,
+        n2.embedding
+      ) AS cosineSimilarity,
+      gds.alpha.linkprediction.adamicAdar(n1, n2, {
+        relationshipQuery: 'NEGATIVE'
+      }) AS negative,
+      gds.alpha.linkprediction.adamicAdar(n1, n2, {
+        relationshipQuery: 'FRIENDS'
+      }) AS friends,
+      coalesce(md1.gender, n1.type) as t1, 
+      coalesce(md2.gender, n2.type) as t2,
+      coalesce(srel0.score, 0) as sim0,
+      coalesce(srel1.score, 0) as sim1,
+      coalesce(srel2.score, 0) as sim2,
+      coalesce(srel3.score, 0) as sim3,
+      coalesce(srel4.score, 0) as sim4
+      WHERE n1 <> n2
+      RETURN 
+      // n1.userId as u1,
+      // n2.userId as u2,
+      t1,
+      t2,
+      // t1 <> t2 as diff,
+      // cosineSimilarity as c,
+      round(sim0,3) as sim0,
+      round(sim1,3) as sim1,
+      round(sim2,3) as sim2,
+      round(sim3,3) as sim3,
+      round(sim4,3) as sim4,
+      // ,
+      // negative as n,
+      // friends as f,
+      sim2 - sim4 as score
+      ORDER BY score DESC
+    `,
+    );
+
+    printResults(results, 100);
 
     return;
 
