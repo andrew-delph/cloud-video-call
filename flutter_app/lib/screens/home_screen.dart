6c063b346042fee12c3fa8e16c9e222daee582e6
Andrew Delph <andrewdelph@live.ca>
2023-05-20 18:15:40 -0400

    clean up homescreen

--- flutter_app/lib/screens/home_screen.dart
+++ flutter_app/lib/screens/home_screen.dart
@@ -7,104 +7,10 @@ import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
 import '../routes/app_pages.dart';
-import '../utils/utils.dart';
 import '../widgets/SwipeDetector.dart';
 
 class HomeScreen extends GetView<HomeController> {
-  Set<int> dialogLock = {};
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //       appBar: AppBar(
-  //         automaticallyImplyLeading: false,
-  //         title: Text("home screen..."),
-  //         actions: <Widget>[
-  //           IconButton(
-  //             icon: const Icon(Icons.settings),
-  //             tooltip: 'Options',
-  //             onPressed: () {
-  //               Get.toNamed(Routes.OPTIONS);
-  //             },
-  //           ),
-  //           IconButton(
-  //             icon: const Icon(Icons.logout),
-  //             tooltip: 'Logout',
-  //             onPressed: () async {
-  //               Get.find<AuthService>().signOut();
-  //               Get.offAllNamed(Routes.LOGIN);
-  //             },
-  //           ),
-  //         ],
-  //       ),
-  //       body: controller.obx(
-  //         (state) => const Center(
-  //           child: Text("this is the home..."),
-  //         ),
-  //         onLoading: const CircularProgressIndicator(),
-  //         onEmpty: Column(
-  //           children: const [
-  //             Text('No Data found'),
-  //           ],
-  //         ),
-  //       ));
-  // }
-
-  // @override
-  // void initState() {
-  //   super.initState();
-  //   controller controller = context.read<controller>();
-
-  //   WidgetsBinding.instance.addPostFrameCallback((_) {
-  //     controller.socketMachine.current = SocketStates.connecting;
-  //     controller.init(handleErrorCallback: handleError);
-  //   });
-
-  //   print("ChatScreenState initState");
-  // }
-
-  void showDialogAlert(int lockID, Widget title, Widget content) {
-    // if (dialogLock.contains(lockID) == true) return;
-    // if (lockID > 0) dialogLock.add(lockID);
-    // func() => showDialog(
-    //       context: context,
-    //       builder: (BuildContext context) {
-    //         return AlertDialog(
-    //           title: title,
-    //           content: content,
-    //           actions: <Widget>[
-    //             TextButton(
-    //               child: const Text("OK"),
-    //               onPressed: () {
-    //                 dialogLock.remove(lockID);
-    //                 Navigator.of(context).pop();
-    //               },
-    //             ),
-    //           ],
-    //         );
-    //       },
-    //     );
-    // // Future.delayed(const Duration(seconds: 0), func);
-    // Future.microtask(func);
-  }
-
-  void handleError(ErrorDetails details) {
-    var content = IntrinsicHeight(
-      child: Column(
-        children: [
-          Text(details.title),
-          const Text(""),
-          Text(
-              style: const TextStyle(
-                color: Colors.red,
-              ),
-              details.message),
-        ],
-      ),
-    );
-    showDialogAlert(
-        details.message.hashCode, const Text("Socket Error"), content);
-  }
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
