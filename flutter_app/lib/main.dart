7bb8b0c0c8078165ffe5358dd99373ad9095043f
Andrew Delph <andrewdelph@live.ca>
2023-05-20 21:04:30 -0400

    remove bad import

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -4,7 +4,6 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_app/screens/login_screen.dart';
-import 'package:flutter_app/screens/main_screen.dart';
 import 'package:get/get.dart';
 import 'package:provider/provider.dart';
 
