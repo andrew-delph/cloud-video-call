6ff9143229d1e4a3e57cb0e8dd29527580fa8a47
Andrew Delph <andrewdelph@live.ca>
2023-05-23 09:24:33 -0400

    options binding

--- flutter_app/lib/bindings/options_binding.dart
+++ flutter_app/lib/bindings/options_binding.dart
@@ -1,9 +1,15 @@
 // Package imports:
 import 'package:get/get.dart';
 
+import '../controllers/options_controller.dart';
+import '../services/options_service.dart';
+
 // Project imports:
 
 class OptionsBinding extends Bindings {
   @override
-  void dependencies() {}
+  void dependencies() {
+    Get.put<PreferencesController>(
+        PreferencesController(Get.put<OptionsService>(OptionsService())));
+  }
 }
