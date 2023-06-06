c4a5c9f960d448a741baf4ce45aec4b7ea80646c
Andrew Delph <andrewdelph@live.ca>
2023-06-05 23:41:00 -0400

    update k6

--- k6/src/k6.ts
+++ k6/src/k6.ts
@@ -9,7 +9,7 @@ import exec from 'k6/execution';
 import { User, userFunctions } from './User';
 
 const vus = 30;
-const authKeysNum = vus +10; // number of users created for each parallel instance running
+const authKeysNum = vus *2; // number of users created for each parallel instance running
 const iterations = authKeysNum * 1000;
 
 const nukeData = true; // this doesnt work with multile running instances
@@ -17,10 +17,10 @@ const uniqueAuthIds = true; //for every test new auth will be created
 const shuffleUsers = true; // shuffle the users to insert redis
 const updatePreferences = true; // update attributes/filters in neo4j
 
-const validMatchChatTime = 0 * 60; //60 * 5; // number of seconds to delay if valid match
+const validMatchChatTime = 0; //60 * 5; // number of seconds to delay if valid match
 const invalidMatchChatTime = 0; //60 * 5;
 
-const matches = 1//Infinity; // number of matches per vus. -1 is inf
+const matches = 5//Infinity; // number of matches per vus. -1 is inf
 
 let runnerId = ``;
 let uniqueAuthKey = ``;
@@ -32,9 +32,9 @@ userFunctions.push(usersLib.createFemale);
 userFunctions.push(usersLib.createMale);
 userFunctions.push(usersLib.createGroupA);
 userFunctions.push(usersLib.createGroupB);
-for (let i = 0; i < usersLib.hotRange / 3; i++) {
-  userFunctions.push(usersLib.createHot);
-}
+// for (let i = 0; i < usersLib.hotRange / 3; i++) {
+//   userFunctions.push(usersLib.createHot);
+// }
 
 const updateAuthVars = () => {
   if (uniqueAuthIds) {
