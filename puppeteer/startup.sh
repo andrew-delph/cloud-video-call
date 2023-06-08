1e3bd1ce6483ceec5fc35545bb1616c4566cb398
Andrew Delph <andrewdelph@live.ca>
2023-06-08 16:39:58 -0400

    working vpn no work proxy

--- puppeteer/startup.sh
+++ puppeteer/startup.sh
@@ -1,26 +1,67 @@
 #!/bin/bash
 set -e
+sleep 2
 
-echo "dns before"
-cat /etc/resolv.conf
-resolv_conf=$(cat /etc/resolv.conf)
-echo "nameserver 8.8.8.8" > /etc/resolv.conf
-echo "$resolv_conf" >> /etc/resolv.conf
-echo "dns after"
-cat /etc/resolv.conf
+function test_commands() {
+    echo
+    echo "--- test_commands ---"
+    echo
+    # echo .......
+    # dig options.default
+
+    nslookup options.default | grep 'Server:'
+
+    echo "--curl--"
+    curl -sS https://2ip.io
+    curl -sS http://options.default/health
+    
+    # cat /etc/resolv.conf
+    # # echo
+    # ip addr
+    # ping -c 4 10.244.2.111
+    # ip link show eth0
+    # curl --interface eth0 --max-time 10 -H "Host: options.default" http://10.107.5.46/health
+
+    echo
+    echo
+}
+
+
+test_commands
+
+
+# echo "dns before"
+# cat /etc/resolv.conf
+# # resolv_conf=$(cat /etc/resolv.conf)
+# # echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+# # echo "$resolv_conf" >> /etc/resolv.conf
+# echo "" > /etc/resolv.conf
+# # echo "nameserver 8.8.8.8"" >> /etc/resolv.conf
+# echo "nameserver 10.96.0.10" >> /etc/resolv.conf
+# echo "search bot.svc.cluster.local svc.cluster.local cluster.local home" >> /etc/resolv.conf
+# echo "options ndots:5" >> /etc/resolv.conf
+# echo
+# echo "dns after"
+# cat /etc/resolv.conf
+
+# echo "dns before"
+# cat /etc/resolv.conf
+# resolv_conf=$(cat /etc/resolv.conf)
+# echo "nameserver 8.8.8.8" > /etc/resolv.conf
+# echo "$resolv_conf" >> /etc/resolv.conf
+# echo "dns after"
+# cat /etc/resolv.conf
+
 
 mkdir -p /dev/net 
 mknod /dev/net/tun c 10 200 
 chmod 600 /dev/net/tun
 
-curl -sS https://2ip.io
+test_commands
 
 echo "starting openvpn"
-
-
 # Get a list of files with the .test extension in the test directory
 files=(/pia-openvpn/*.ovpn)
-
 # Check if there are any .test files in the directory
 if [ ${#files[@]} -eq 0 ]; then
   echo "No .ovpn files found."
@@ -32,9 +73,13 @@ random_opvn=${files[random_index]}
 
 echo "Randomly selected file: $random_opvn"
 
+# echo "route-nopull" >> $random_opvn
 echo "auth-user-pass '/pia-openvpn/auth-user-pass.txt'" >> $random_opvn
+# echo "route 10.96.0.0 255.255.0.0 net_gateway" >> $random_opvn
+echo "route 10.96.0.0 255.240.0.0 net_gateway" >> $random_opvn
 
-openvpn --config $random_opvn &
+
+openvpn --config $random_opvn >/dev/null 2>&1 &
 
 counter=0
 while true; do
@@ -49,16 +94,16 @@ while true; do
         echo "VPN is connected"
         break
     else
-        echo "VPN is not yet connected. Retrying in 1 second."
+        # echo "VPN is not yet connected. Retrying in 1 second."
         sleep 1
         counter=$((counter + 1))
     fi
 done
 
+test_commands
 
-# nslookup google.com
-
-curl -sS https://2ip.io
+# echo exiting...
+# exit 1
 
 /hola-proxy -bind-address 0.0.0.0:8080 -proxy-type peer &
 
@@ -75,13 +120,11 @@ while true; do
         echo "Proxy is ready"
         break
     else
-        echo "Proxy is not yet ready. Retrying in 1 second."
+        # echo "Proxy is not yet ready. Retrying in 1 second."
         sleep 1
         counter=$((counter + 1))
     fi
 done
-
-curl -sS https://2ip.io
 
 if [ ! -v RUN_COMMAND ] || [ -v PROXY ]; then
     echo "RUN_COMMAND=$RUN_COMMAND PROXY=$PROXY";
