01d06ea72a6ec824d06ee9bacd7e096d9230bcc4
Andrew Delph <andrewdelph@live.ca>
2023-06-12 20:03:40 -0400

    5 min cooldown

--- matchmaker/src/matchmaker.ts
+++ matchmaker/src/matchmaker.ts
@@ -73,7 +73,7 @@ const maxCooldownAttemps = Math.floor(
   maxCooldownDelay ** (1 / cooldownScalerValue),
 );
 
-const lastMatchedCooldownMinutes = 10; // filter of last matches
+const lastMatchedCooldownMinutes = 5; // filter of last matches
 const recentMatchesLowerScore = false;
 
 const relationShipScoresSortFunc = (
