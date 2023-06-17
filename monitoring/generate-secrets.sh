ae6282da23d4efeada3d16ffb4e84db5bca47c1e
Andrew Delph <andrewdelph@live.ca>
2023-06-17 16:52:41 -0400

    monitoring generate-secrets.sh

---
+++ monitoring/generate-secrets.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+OUTPUT=$(base64 -w0 alertmanager.yml)
+echo "$OUTPUT"
+
+sed -e "s|alertmanager.yml:.*|alertmanager.yml: $OUTPUT|"  secrets.yaml | kubectl apply -f -
+# | kubectl apply  -f - -n k6
