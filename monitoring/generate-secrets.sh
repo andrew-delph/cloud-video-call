11fb7677a7ce1b3a02eae06d1abe6fbbdcf62623
Andrew Delph <andrewdelph@live.ca>
2023-06-17 17:53:51 -0400

    no alert manager for minikube

--- monitoring/generate-secrets.sh
+++ monitoring/generate-secrets.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -e
+
+# Run the 'kubectl ctx' command and capture the output
+ctx=$(kubectl config current-context)
+
+# Check if the context is "minikube"
+if echo "$ctx" | grep -q "minikube"; then
+  echo "Minikube context detected. Exiting..."
+  exit 1
+fi
+
 OUTPUT=$(base64 -w0 alertmanager.yml)
 echo "$OUTPUT"
 
