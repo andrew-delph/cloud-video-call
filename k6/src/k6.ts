a218216514d1eef36074421ac2f1b5a5af83b9a6
Andrew Delph <andrewdelph@live.ca>
2023-06-22 13:39:39 -0400

    change vus to 100

--- k6/src/k6.ts
+++ k6/src/k6.ts
@@ -8,7 +8,7 @@ import redis from 'k6/experimental/redis';
 import http from 'k6/http';
 import { Counter, Rate, Trend, Gauge } from 'k6/metrics';
 
-const vus = 200;
+const vus = 100;
 const authKeysNum = vus + 500; // number of users created for each parallel instance running
 const iterations = 999999; //authKeysNum * 1000;
 
