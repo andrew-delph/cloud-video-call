be29dd3d17c1fe86e3986950a787b26e1234e440
Andrew Delph <andrewdelph@live.ca>
2023-07-04 19:34:26 -0400

    profile photo use CachedNetworkImage

--- flutter_app/lib/widgets/profile_picture.dart
+++ flutter_app/lib/widgets/profile_picture.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 import 'dart:typed_data';
 
 // Flutter imports:
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 // Package imports:
@@ -16,6 +17,7 @@ class ProfilePicture extends GetView<PreferencesController> {
   ProfilePicture(this.userId, {super.key});
 
   final String userId;
+  final RxString photoUrl = "".obs;
 
   final Rx<Uint8List?> bytes = Rx<Uint8List?>(null);
 
@@ -24,22 +26,30 @@ class ProfilePicture extends GetView<PreferencesController> {
     var imageRef =
         (FirebaseStorage.instance.ref('profile-picture/${userId}_100x100'));
 
-    imageRef.getData().then((value) => bytes(value)).catchError((onError) {
-      log("profile picture not found");
+    imageRef
+        .getDownloadURL()
+        .then((value) => photoUrl(value))
+        .catchError((err) {
+      print("no profile photo: $err");
     });
 
     return Obx(() {
-      Uint8List? imageBytes = bytes();
-
       return Row(
         children: [
           SizedBox(
             height: 100,
             width: 100,
-            child: imageBytes != null
-                ? Image(image: MemoryImage(imageBytes))
-                : const Text("No Profile Picture"),
-          ),
+            child: photoUrl().isEmpty
+                ? const Icon(Icons.no_photography_sharp)
+                : CachedNetworkImage(
+                    imageUrl: photoUrl(),
+                    placeholder: (context, url) =>
+                        const CircularProgressIndicator(),
+                    errorWidget: (context, url, error) {
+                      return const Icon(Icons.no_photography_sharp);
+                    },
+                  ),
+          )
         ],
       );
     });
--- flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ flutter_app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -15,6 +15,7 @@ import flutter_webrtc
 import geolocator_apple
 import package_info_plus
 import path_provider_foundation
+import sqflite
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
@@ -27,4 +28,5 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
 }
