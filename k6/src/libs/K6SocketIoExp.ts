a4e518d1b8a0c322d821ae016245cd52b99e4505
Andrew Delph <andrewdelph@live.ca>
2023-07-07 15:54:54 -0400

    typoes

--- k6/src/libs/K6SocketIoExp.ts
+++ k6/src/libs/K6SocketIoExp.ts
@@ -1,10 +1,10 @@
 import { K6SocketIoBase } from './K6SocketIoBase';
-import { socketio-service } from 'k6/experimental/socketio-services';
+import { WebSocket } from 'k6/experimental/websockets';
 
 export class K6SocketIoExp extends K6SocketIoBase {
   connect(): void {
     const socketIo = this;
-    socketIo.setSocket(new socketio-service(this.url, null, this.params));
+    socketIo.setSocket(new WebSocket(this.url, null, this.params));
     this.socket.addEventListener(`open`, () => {});
   }
   on(event: string, callback: (data: any) => void): void {
