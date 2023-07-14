7a4bf61ac6240d164ceecc5364e846cbc0afad31
Andrew Delph <andrewdelph@live.ca>
2023-07-14 13:20:07 -0400

    data-service update image_push

--- data-service/main.go
+++ data-service/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"log"
 	"net"
 
@@ -10,36 +12,85 @@ import (
 
 	pb "github.com/andrew-delph/cloud-video-call/common-messaging/proto"
 )
-const (
-	port = ":50051"
+
+var (
+	port = flag.Int("port", 80, "The server port")
 )
 
+type server struct {
+	pb.UnimplementedDataServiceServer
+}
 
-// type server struct {
-// 	pb.UnimplementedGreeterServer
-// }
+
+func (s *server) CreateUser(ctx context.Context, in *pb.CreateUserRequest) (*pb.CreateUserResponse, error) {
+	log.Printf("Received: %v")
+	return &pb.CreateUserResponse{}, nil
+}
+
+func (s *server) CreateMatch(ctx context.Context, in *pb.CreateMatchRequest) (*pb.CreateMatchResponse, error) {
+	log.Printf("Received: %v")
+	return &pb.CreateMatchResponse{}, nil
+}
+
+func (s *server) EndCall(ctx context.Context, in *pb.EndCallRequest) (*pb.StandardResponse, error) {
+	log.Printf("Received: %v")
+	return &pb.StandardResponse{}, nil
+}
+
+func (s *server) CreateFeedback(ctx context.Context, in *pb.CreateFeedbackRequest) (*pb.Match, error) {
+	log.Printf("Received: %v")
+	return &pb.Match{}, nil
+}
+
+
+func (s *server) GetRelationshipScores(ctx context.Context, in *pb.GetRelationshipScoresRequest) (*pb.GetRelationshipScoresResponse, error) {
+	log.Printf("Received: %v")
+	return &pb.GetRelationshipScoresResponse{}, nil
+}
+
+
+func (s *server) CheckUserFilters(ctx context.Context, in *pb.CheckUserFiltersRequest) (*pb.CheckUserFiltersResponse, error) {
+	log.Printf("Received: %v")
+	return &pb.CheckUserFiltersResponse{}, nil
+}
+
+func (s *server) UpdatePerferences(ctx context.Context, in *pb.UpdatePerferencesRequest) (*pb.StandardResponse, error) {
+	log.Printf("Received: %v")
+	return &pb.StandardResponse{}, nil
+}
+
+func (s *server) GetUserPerferences(ctx context.Context, in *pb.GetUserPerferencesRequest) (*pb.GetUserPerferencesResponse, error) {
+	log.Printf("Received: %v")
+	return &pb.GetUserPerferencesResponse{}, nil
+}
+
+func (s *server) PutUserPerferences(ctx context.Context, in *pb.PutUserPerferencesRequest) (*pb.PutUserPerferencesResponse, error) {
+	log.Printf("Received: %v")
+	return &pb.PutUserPerferencesResponse{}, nil
+}
+
+func (s *server) GetMatchHistory(ctx context.Context, in *pb.MatchHistoryRequest) (*pb.MatchHistoryResponse, error) {
+	log.Printf("Received: %v")
+	return &pb.MatchHistoryResponse{}, nil
+}
+
+func (s *server) InsertUserVectors(ctx context.Context, in *pb.InsertUserVectorsRequest) (*pb.StandardResponse, error) {
+	log.Printf("Received: %v")
+	return &pb.StandardResponse{}, nil
+}
+
 
 
 func main() {
-	fmt.Println("Starting...")
-}
-
-func start() {
-	fmt.Println("Starting...")
-	lis, err := net.Listen("tcp", port)
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
+	pb.RegisterDataServiceServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
 	}
-}
-
-func test() (*pb.StandardResponse, error){
-	return &pb.StandardResponse{}, nil
-}
-
-func test2() (*pb.MatchMessage, error){
-	return &pb.MatchMessage{}, nil
 }
