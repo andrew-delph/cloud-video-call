1a672755aa308af6753e17a5724ea0dc9b8c2fc2
Andrew Delph <andrewdelph@live.ca>
2024-04-22 11:05:37 -0400

    events testing compile

---
+++ socketio-event/example1.ts
@@ -0,0 +1,5 @@
+import { myFunc } from './example2';
+
+console.log(`hELOOOOOOOOO`);
+
+myFunc();
---
+++ socketio-event/example2.ts
@@ -0,0 +1,4 @@
+console.log(`this is example2`);
+export function myFunc() {
+  console.log(`CALLING myFunc`);
+}
